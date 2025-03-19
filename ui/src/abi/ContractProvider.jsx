d0340ec7b61a0e644d7a62a17b5f3a9af1950c20
soif2sang <sirefight.gaming@gmail.com>
2025-03-19 13:14:11 +0100

    Update the Provider

--- ui/src/abi/ContractProvider.jsx
+++ ui/src/abi/ContractProvider.jsx
@@ -20,7 +20,7 @@ export const ContractProvider = ({ children }) => {
         async function loadContract() {
             try {
                 const contractInstance = new ethers.Contract(
-                    '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+                    '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
                     voting.abi,
                     signer
                 );

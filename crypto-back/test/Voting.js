6a3323f59db9b5168659ab009f61bde10b08e1b1
Sontag Maxime <maxime_sontag@ens.univ-artois.fr>
2025-03-20 11:36:37 +0100

    Tests automatisés

--- crypto-back/test/Voting.js
+++ crypto-back/test/Voting.js
@@ -1,311 +1,188 @@
-const { ethers } = require("hardhat");
-const {
-	time,
-	loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-const IS_REGISTERED = 0;
-const HAS_VOTED = 1;
-const VOTED_PROPOSAL_ID = 2;
+describe("Voting Contract", function () {
+    let Voting;
+    let voting;
+    let owner;
+    let addr1;
+    let addr2;
+    let addr3;
 
-const REGISTERING_VOTERS = 0;
-const PROPOSALS_REGISTRATION_STARTED = 1;
-const PROPOSALS_REGISTRATION_ENDED = 2;
-const VOTING_SESSION_STARTED = 3;
-const VOTING_SESSION_ENDED = 4;
-const VOTES_TALLIED = 5;
+    beforeEach(async () => {
+        [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
-const DESCRIPTION = 0;
-const VOTE_COUNT = 1;
+        // Déployer le contrat
+        Voting = await ethers.getContractFactory("Voting");
+        voting = await Voting.deploy(owner.address);  // Déployer le contrat
+    });
 
-describe("Voting", function () {
-	async function getSomeAccounts() {
-		const [firstAccount, secondAccount, thirdAccount, fourthAccount] = await ethers.getSigners();
-		return { firstAccount, secondAccount, thirdAccount, fourthAccount };
-	}
+    describe("Voter registration", function () {
+        it("should allow the owner to register a voter", async function () {
+            await voting.openVotersRegistration(); // Ouvrir la phase d'enregistrement des électeurs
+            await voting.registerVoter(addr1.address); // Inscrire un électeur
 
-	async function deploy(account) {
-		const Voting = await ethers.getContractFactory("Voting");
-		const voting = await Voting.deploy(account);
+            const voter = await voting.whitelist(addr1.address);
+            expect(voter.isRegistered).to.be.true;
+            expect(voter.hasVoted).to.be.false;
+        });
 
-		return voting;
-	}
-
-	describe("Deployment", function () {
-		it("Should say hello world", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
-
-			expect(await voting.helloWorld()).to.equal("Hello World!");
+		/** Bloque car renvoie une custom error
+        it("should not allow a non-owner to register a voter", async function () {
+			await expect(voting.connect(addr1).registerVoter(addr2.address))
+				.to.be.revertedWith("Ownable: caller is not the owner");
 		});
-		it("Admin should be contract deployer", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
+		*/
 
-			expect(await voting.getAdministrator()).to.equal((await getSomeAccounts()).firstAccount.address);
+        it("should revert if trying to register a voter outside the registration phase", async function () {
+			await voting.closeRegistration(); 
+		
+			await expect(voting.registerVoter(addr1.address))
+				.to.be.revertedWith("Voters registration is not open");
 		});
-	});
-
-	describe("Administrator adds voters", function () {
-		it("Should be empty", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
-
-			expect(await voting.votersCount()).to.equal(0);
-		});
-		it("Should add one voter", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
-
-			await expect(voting.addVoter(firstAccount.address))
-				.to
-				.emit(voting, "VoterRegistered")
-				.withArgs(firstAccount.address);
-
-			expect(await voting.votersCount()).to.equal(1);
+    });
 
-			const voter = await voting.voters(firstAccount.address);
-			expect(voter[IS_REGISTERED]).to.equal(false);
-			expect(voter[HAS_VOTED]).to.equal(false);
-			expect(voter[VOTED_PROPOSAL_ID]).to.equal(0);
-		});
-	});
-
-	describe("Administrator opens proposals registering", function () {
+    describe("Proposal registration", function () {
+        it("should allow registered voters to register proposals", async function () {
+            await voting.openVotersRegistration(); // Ouvrir la phase d'enregistrement des électeurs
+            await voting.registerVoter(addr1.address);
 
-		it("Should have 'RegisteringVoters' as first status", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
-			expect(await voting.status()).to.equal(REGISTERING_VOTERS);
-		});
+            await voting.openRegistration(); // Ouvrir la phase d'enregistrement des propositions
+            await voting.connect(addr1).registerProposal("Proposal 1");
 
-		it("Should have 'ProposalsRegistrationStarted' when opening proposals registering", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
+            const proposals = await voting.getProposals();
+            expect(proposals.length).to.equal(1);
+            expect(proposals[0].description).to.equal("Proposal 1");
+        });
 
-			await expect(voting.nextStatus())
-				.to
-				.emit(voting, "WorkflowStatusChange")
-				.withArgs(REGISTERING_VOTERS, PROPOSALS_REGISTRATION_STARTED);
-		});
+        it("should not allow non-registered voters to register proposals", async function () {
+            await expect(
+                voting.connect(addr2).registerProposal("Proposal 2")
+            ).to.be.revertedWith("Proposals registration is not open");
+        });
 
-		describe("Voters can propose while proposals registration is opened", function () {
-			it("Should not allow proposing when proposals registration is not opened", async function () {
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
+        it("should revert if the proposal is already registered", async function () {
+            await voting.openVotersRegistration();
+            await voting.registerVoter(addr1.address);
 
-				await expect(voting.propose("description"))
-					.not.to.emit(voting, "ProposalRegistered");
-			});
+            await voting.openRegistration();
+            await voting.connect(addr1).registerProposal("Proposal 1");
 
-			it("Should allow proposing when proposals registration is opened", async function () {
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
+            await expect(
+                voting.connect(addr1).registerProposal("Proposal 1")
+            ).to.be.revertedWith("Proposal already registered");
+        });
+    });
 
-				await voting.nextStatus();
+    describe("Voting", function () {
+        it("should allow registered voters to vote", async function () {
+            await voting.openVotersRegistration();
+            await voting.registerVoter(addr1.address);
 
-				await expect(voting.propose("description"))
-					.to
-					.emit(voting, "ProposalRegistered")
-					.withArgs(1);
+            await voting.openRegistration();
+            await voting.connect(addr1).registerProposal("Proposal 1");
 
-				expect((await voting.proposals(1))[DESCRIPTION]).to.equal("description");
-				expect((await voting.proposals(1))[VOTE_COUNT]).to.equal(0);
-			});
+            await voting.openVoting();
+            await voting.connect(addr1).vote(0);
 
-			it("Should not allow proposing when proposals registration is closed", async function () {
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
+            const proposals = await voting.getProposals();
+            expect(proposals[0].voteCount).to.equal(1);
+        });
 
-				await voting.nextStatus();
-				await voting.nextStatus();
+        it("should not allow voters to vote multiple times", async function () {
+            await voting.openVotersRegistration();
+            await voting.registerVoter(addr1.address);
 
-				await expect(voting.propose("description"))
-					.not.to.emit(voting, "ProposalRegistered");
-			});
-		});
+            await voting.openRegistration();
+            await voting.connect(addr1).registerProposal("Proposal 1");
 
-		it("Should have 'ProposalsRegistrationEnded' when closing proposals registering", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
+            await voting.openVoting();
+            await voting.connect(addr1).vote(0);
 
-			await voting.nextStatus();
+            await expect(voting.connect(addr1).vote(0)).to.be.revertedWith(
+                "Voter already voted"
+            );
+        });
 
-			await expect(voting.nextStatus())
-				.to
-				.emit(voting, "WorkflowStatusChange")
-				.withArgs(PROPOSALS_REGISTRATION_STARTED, PROPOSALS_REGISTRATION_ENDED);
-		});
-	});
+        it("should not allow non-registered voters to vote", async function () {
+            await voting.openVotersRegistration();
+            await voting.registerVoter(addr1.address);
 
-	describe("Administrator opens voting session", function () {
-		it("Should not allow voting when voting session has not started yet", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
+            await voting.openRegistration();
+            await voting.connect(addr1).registerProposal("Proposal 1");
 
-			await expect(voting.vote(2))
-				.not.to.emit(voting, "ProposalRegistered");
-		});
+            await voting.openVoting();
 
-		describe("Voters can vote while voting session is open", function () {
+            await expect(voting.connect(addr2).vote(0)).to.be.revertedWith(
+                "Voter is not registered"
+            );
+        });
 
-			it("Should not allow voting for non-existing proposal", async function () {
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
+        it("should allow voters to unvote", async function () {
+            await voting.openVotersRegistration();
+            await voting.registerVoter(addr1.address);
 
-				await voting.addVoter(firstAccount);
-				await voting.addVoter(secondAccount);
+            await voting.openRegistration();
+            await voting.connect(addr1).registerProposal("Proposal 1");
 
-				await voting.nextStatus();
+            await voting.openVoting();
+            await voting.connect(addr1).vote(0);
 
-				await voting.propose("description");
+            await voting.connect(addr1).unvote();
 
-				await voting.nextStatus();
-				await voting.nextStatus();
+            const proposals = await voting.getProposals();
+            expect(proposals[0].voteCount).to.equal(0);
+        });
+    });
 
-				await expect(voting.vote(2))
-					.not.to
-					.emit(voting, "Voted");
-			});
+    describe("Workflow control", function () {
+        it("should allow the owner to control the workflow", async function () {
+            await voting.openVotersRegistration();
+            expect(await voting.getWorkflowStatus()).to.equal(0); // RegisteringVoters
 
-			it("Should vote", async function () {
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
-
-				await voting.addVoter(secondAccount);
-				await voting.addVoter(thirdAccount);
-				await voting.addVoter(fourthAccount);
-
-				await voting.nextStatus();
-
-				await voting.proposeTest(secondAccount, "description");
-
-				await voting.nextStatus();
-				await voting.nextStatus();
-
-				await expect(voting.voteTest(thirdAccount, 1))
-					.to
-					.emit(voting, "Voted")
-					.withArgs(thirdAccount.address, 1);
-
-				expect((await voting.voters(thirdAccount.address))[VOTED_PROPOSAL_ID]).to.equal(1);
-				expect((await voting.voters(thirdAccount.address))[HAS_VOTED]).to.equal(true);
-
-				expect((await voting.proposals(1))[VOTE_COUNT]).to.equal(1);
-				expect((await voting.proposals(1))[DESCRIPTION]).to.equal("description");
-			});
-
-
-		});
-
-		it("Should not allow voting when voting session is closed", async function () {
-			const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-			const voting = await deploy(firstAccount);
-
-			await voting.nextStatus();
-			await voting.nextStatus();
-			await voting.nextStatus();
-			await voting.nextStatus();
-
-			await expect(voting.vote(2))
-				.not.to.emit(voting, "ProposalRegistered");
-		});
-
-		describe("Administrator counts votes", function () {
-
-			it("Should have 'VotesTallied' when counting votes", async function () {
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
-
-				await voting.nextStatus();
-				await voting.nextStatus();
-				await voting.nextStatus();
-				await voting.nextStatus();
-
-				expect(await voting.status()).to.equal(VOTING_SESSION_ENDED);
-
-				await voting.nextStatus();
-
-				expect(await voting.status()).to.equal(VOTES_TALLIED);
-			});
-
-			it("Should count votes by proposal", async function () {
-				// secondAccount and thirdAccount are proposing
-				// secondAccount votes for thirdAccount's proposal
-				// thirdAccount votes for secondAccount's proposal
-				// fourthAccount votes for secondAccount's proposal
-				// secondAccount's proposal should win with 2 votes, thirdAccount's proposal should lose with 1 vote
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
-
-				await voting.addVoter(secondAccount);
-				await voting.addVoter(thirdAccount);
-				await voting.addVoter(fourthAccount);
-
-				await voting.nextStatus();
-
-				await voting.proposeTest(secondAccount, "second account description");
-				await voting.proposeTest(thirdAccount, "third account description");
-
-				await voting.nextStatus();
-				await voting.nextStatus();
-
-				const SECOND_ACCOUNT_PROPOSAL = 1;
-				const THIRD_ACCOUNT_PROPOSAL = 2;
-
-				await voting.voteTest(secondAccount, THIRD_ACCOUNT_PROPOSAL);
-				await voting.voteTest(thirdAccount, SECOND_ACCOUNT_PROPOSAL);
-				await voting.voteTest(fourthAccount, SECOND_ACCOUNT_PROPOSAL);
-
-				expect((await voting.proposals(THIRD_ACCOUNT_PROPOSAL))[VOTE_COUNT]).to.equal(1);
-				expect((await voting.proposals(SECOND_ACCOUNT_PROPOSAL))[VOTE_COUNT]).to.equal(2);
-
-				await voting.nextStatus();
-				await voting.nextStatus();
-
-				expect(await voting.winningProposalId()).to.equal(SECOND_ACCOUNT_PROPOSAL);
-			});
-
-			it("Should set the winner only when status becomes 'VotesTallied'", async function () {
-				// secondAccount and thirdAccount are proposing
-				// secondAccount votes for thirdAccount's proposal
-				// thirdAccount votes for secondAccount's proposal
-				// fourthAccount votes for secondAccount's proposal
-				// secondAccount's proposal should win with 2 votes, thirdAccount's proposal should lose with 1 vote
-				const { firstAccount, secondAccount, thirdAccount, fourthAccount } = await getSomeAccounts();
-				const voting = await deploy(firstAccount);
-
-				expect(await voting.winningProposalId()).to.equal(0);
-
-				await voting.addVoter(secondAccount);
-				await voting.addVoter(thirdAccount);
-				await voting.addVoter(fourthAccount);
-
-				await voting.nextStatus();
-
-				await voting.proposeTest(secondAccount, "second account description");
-				await voting.proposeTest(thirdAccount, "third account description");
-
-				await voting.nextStatus();
-				await voting.nextStatus();
-
-				const SECOND_ACCOUNT_PROPOSAL = 1;
-				const THIRD_ACCOUNT_PROPOSAL = 2;
-
-				await voting.voteTest(secondAccount, THIRD_ACCOUNT_PROPOSAL);
-				await voting.voteTest(thirdAccount, SECOND_ACCOUNT_PROPOSAL);
-				await voting.voteTest(fourthAccount, SECOND_ACCOUNT_PROPOSAL);
-				await voting.nextStatus();
-
-				expect(await voting.winningProposalId()).to.equal(0);
-
-				await voting.nextStatus();
-
-				expect(await voting.winningProposalId()).to.equal(SECOND_ACCOUNT_PROPOSAL);
-			});
-		});
-
-	});
-});
+            await voting.openRegistration();
+            expect(await voting.getWorkflowStatus()).to.equal(1); // ProposalsRegistrationStarted
+
+            await voting.closeRegistration();
+            expect(await voting.getWorkflowStatus()).to.equal(2); // ProposalsRegistrationEnded
+
+            await voting.openVoting();
+            expect(await voting.getWorkflowStatus()).to.equal(3); // VotingSessionStarted
+
+            await voting.closeVoting();
+            expect(await voting.getWorkflowStatus()).to.equal(4); // VotingSessionEnded
+
+            await voting.tallyVotes();
+            expect(await voting.getWorkflowStatus()).to.equal(5); // VotesTallied
+        });
+
+		/** Bloque car renvoie une custom error
+        it("should not allow non-owner to change the workflow", async function () {
+            await expect(
+                voting.connect(addr1).openVotersRegistration()
+            ).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+		*/
+    });
+
+    describe("Getters", function () {
+        it("should return the correct winner after tallying votes", async function () {
+            await voting.openVotersRegistration();
+            await voting.registerVoter(addr1.address);
+
+            await voting.openRegistration();
+            await voting.connect(addr1).registerProposal("Proposal 1");
+
+            await voting.openVoting();
+            await voting.connect(addr1).vote(0);
+
+            await voting.closeVoting();
+            await voting.tallyVotes();
+
+            const [winnerId, winnerProposal] = await voting.getWinner();
+            expect(winnerId).to.equal(0);
+            expect(winnerProposal.description).to.equal("Proposal 1");
+        });
+    });
+
+}); 
